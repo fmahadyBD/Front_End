a44e39fcec27579be279f4293fa4e1470b5cd932
Mahady Hasan Fahim <109776849+fmahadyBD@users.noreply.github.com>
2023-04-20 14:12:01 +0600

    Add files via upload

---
+++ HW_1/index.js
@@ -0,0 +1,9 @@
+console.log("hi");
+let name = "fahim";
+let age = 22;
+let studnet = true;
+console.log("hi", name);
+console.log(name);
+document.getElementById("p1").innerHTML = "hello " + name;
+document.getElementById("p2").innerHTML = "Age " + age;
+document.getElementById("p3").innerHTML = "student " + studnet;
